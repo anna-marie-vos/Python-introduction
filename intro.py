9c6da1bcefb24f278c85d8accc2bead331a5a495
anna-marie-vos <amvos@outlook.com>
2017-03-04 15:51:35 +1300

    using the urlmethod

--- intro.py
+++ intro.py
@@ -33,3 +33,13 @@ while True:
     if int(response) % 7 == 0:
         print("you've devided by 7")
         break
+
+from urllib.request import urlopen
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+
+print(story_words)
