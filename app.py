4320f8e1196cb5fca0c985e4a750772988a71cd2
anna-marie-vos <amvos@outlook.com>
2017-03-26 10:48:47 +1300

    flask template html working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #Basic route
 @app.route("/")
 def main():
-    return "welcome"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
