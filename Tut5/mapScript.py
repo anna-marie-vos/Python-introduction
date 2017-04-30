1d0a1d52f481cf1d61480ef4b2f648eafa4c9f43
anna-marie-vos <amvos@outlook.com>
2017-04-30 14:15:26 +1200

    completed map

--- Tut5/mapScript.py
+++ Tut5/mapScript.py
@@ -2,13 +2,16 @@ import folium
 import pandas
 
 df = pandas.read_csv("Volcanoes-USA.txt")
-
-map = folium.Map(location = [45.372,-121.697],zoom_start=6, tiles='Stamen Terrain')
+# .mean takes the average
+map = folium.Map(location = [df['LAT'].mean(), df['LON'].mean()],zoom_start=6, tiles='Stamen Terrain')
 
 def newColor(elevation):
-    if elevation in range(0,1500):
+    minVal = int(min(df['ELEV']))
+    maxVal = int(max(df['ELEV']))
+    incriment = int((maxVal - minVal)/3)
+    if elevation in range(minVal, minVal+incriment ):
         return 'green'
-    elif elevation in range(1500,3000):
+    elif elevation in range(minVal+incriment ,minVal+incriment*2 ):
         return 'orange'
     else:
         return 'red'
