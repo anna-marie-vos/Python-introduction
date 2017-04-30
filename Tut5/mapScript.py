47798ea1890eba2ede1521816ffe8dc210d31115
anna-marie-vos <amvos@outlook.com>
2017-04-30 14:03:08 +1200

    adding function and colors to pins

--- Tut5/mapScript.py
+++ Tut5/mapScript.py
@@ -1,5 +1,19 @@
 import folium
-map = folium.Map(location = [45.372,-121.697],zoom_start=12, tiles='Stamen Terrain')
-folium.Marker(location = [45.3288,-121.6625],popup = 'Mt. Hood Meadows').add_to(map)
-folium.Marker(location = [45.3311,-121.7311],popup = 'Timberlake Lodge').add_to(map)
+import pandas
+
+df = pandas.read_csv("Volcanoes-USA.txt")
+
+map = folium.Map(location = [45.372,-121.697],zoom_start=6, tiles='Stamen Terrain')
+
+def newColor(elevation):
+    if elevation in range(0,1500):
+        return 'green'
+    elif elevation in range(1500,3000):
+        return 'orange'
+    else:
+        return 'red'
+
+for lat, lon, name, elev in zip(df['LAT'],df['LON'],df['NAME'], df['ELEV']):
+    folium.Marker(location = [lat,lon],popup = name,icon=folium.Icon(newColor(elev))).add_to(map)
+
 map.save('test.html')
