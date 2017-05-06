b82015db2684d8c7ad078b2f053be346fcbd14ea
anna-marie-vos <amvos@outlook.com>
2017-05-06 19:04:13 +1200

    adding an html page

--- Tut8/script1.py
+++ Tut8/script1.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "website content goes here"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "this is the home page"
+
 
 if __name__ =="__main__":
     app.run(debug=True)
