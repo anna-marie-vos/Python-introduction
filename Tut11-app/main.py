02234bc117619e1dd4bdfc987703aded1cc7d5c5
anna-marie-vos <amvos@outlook.com>
2017-06-03 18:32:00 +1200

    starting the app tutorial

---
+++ Tut11-app/main.py
@@ -0,0 +1,12 @@
+"""
+This program stores book information:
+title, author, year & ISBN
+
+The user can:
+view all records,
+search an entry,
+add an entry,
+update an entry,
+delete an entry,
+close the app
+"""
