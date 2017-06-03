7b44b96c261eac12e313a4b937af47105a5b415c
anna-marie-vos <amvos@outlook.com>
2017-06-03 18:10:28 +1200

    postgres methods finalised

--- Tut10/script2.py
+++ Tut10/script2.py
@@ -28,4 +28,22 @@ def viewDataBase():
     conn.close()
     return rows
 
+def deleteRow(deletedItem):
+    conn = db.connect("dbname = 'tutorialdb' user='postgres' password='postgres' host='localhost' port=5432")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item = %s", (deletedItem,))
+    conn.commit()
+    conn.close()
+
+# deleteRow('mango')
+
+def updateRow(updatedItem, newAmount, newPrice):
+    conn = db.connect("dbname = 'tutorialdb' user='postgres' password='postgres' host='localhost' port=5432")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity = %s, price = %s WHERE item = %s", ( newAmount, newPrice, updatedItem))
+    conn.commit()
+    conn.close()
+
+updateRow('orange', 100, 4)
+
 print(viewDataBase())
