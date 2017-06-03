b5a25f637dacade1a639232b1b89b0845e8032b4
anna-marie-vos <amvos@outlook.com>
2017-06-03 21:04:04 +1200

    minibook app started

---
+++ Tut11-app/interface.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from backend import *
+
+window = Tk()
+
+titleLabel = Label(window,text = 'Title:')
+titleLabel.grid(row = 0, column = 0)
+
+autherLabel = Label(window,text = 'Auther:')
+autherLabel.grid(row = 0, column = 2)
+
+yearLabel = Label(window,text = 'Year:')
+yearLabel.grid(row = 1, column = 0)
+
+ISBNLabel = Label(window,text = 'ISBN:')
+ISBNLabel.grid(row = 1, column = 2)
+
+titleInput = StringVar()
+titleEntry = Entry(window, textvariable = titleInput)
+titleEntry.grid(row = 0, column = 1)
+
+autherInput = StringVar()
+autherEntry = Entry(window, textvariable = autherInput)
+autherEntry.grid(row = 0, column = 3)
+
+yearInput = StringVar()
+yearEntry = Entry(window, textvariable = yearInput)
+yearEntry.grid(row = 1, column = 1)
+
+ISBNInput = StringVar()
+ISBNEntry = Entry(window, textvariable = ISBNInput)
+ISBNEntry.grid(row = 1, column = 3)
+
+list1 = Listbox(window, height = 6, width = 35)
+list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2)
+
+scroller = Scrollbar(window)
+scroller.grid(row = 2,column = 2, rowspan = 6)
+
+list1.configure(yscrollcommand = scroller.set)
+scroller.configure(command = list1.yview)
+
+viewAllBtn = Button(window, text = "View all", width = 12)
+viewAllBtn.grid(row = 2, column = 3)
+
+searchBtn = Button(window, text = "Search Entry", width = 12)
+searchBtn.grid(row = 3, column = 3)
+
+addBtn = Button(window, text = "Add Entry", width = 12)
+addBtn.grid(row =4, column = 3)
+
+updateBtn = Button(window, text = "Update", width = 12)
+updateBtn.grid(row = 5, column = 3)
+
+deleteBtn = Button(window, text = "Delete", width = 12)
+deleteBtn.grid(row = 6, column = 3)
+
+closeBtn = Button(window, text = "Close", width = 12)
+closeBtn.grid(row = 7, column = 3)
+
+
+window.mainloop()
--- Tut11-app/main.py
+++ Tut11-app/main.py
@@ -2,11 +2,14 @@
 This program stores book information:
 title, author, year & ISBN
 
-The user can:
-view all records,
-search an entry,
-add an entry,
-update an entry,
-delete an entry,
+The user can: view all records,
+search an entry, add an entry,
+update an entry, delete an entry,
 close the app
 """
+from interface import *
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
