967146606311809592f5b044798d926d381adb13
anna-marie-vos <amvos@outlook.com>
2017-06-03 21:27:48 +1200

    db methods working

--- Tut11-app/backend.py
+++ Tut11-app/backend.py
@@ -1,10 +1,38 @@
-import sqlite3
+import sqlite3 as db
 
 def connect():
-    conn = sqlite3.connect("books.db")
+    conn = db.connect("books.db")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year INTEGER, isbn INTEGER)")
     conn.commit()
     conn.close()
 
+def insert(title, author, year, isbn):
+    conn = db.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES(null, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = db.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn = db.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?",(title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
 connect()
+insert("The air","Jane Smith",1925, 5523232)
+
+print(view())
+
+print(search(author ="John Smith"))
